1b706ed434427e52008bcf4fecdc2546670d6599
alexszeliga <alexszeliga@gmail.com>
2019-02-04 20:39:45 -0500

    add react, piped build to server

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,11 +1,6 @@
-var db = require("../models");
+const path = require("path");
 module.exports = function(app) {
-  // Load index page
   app.get("/", function(req, res) {
-    res.send("Hello World");
-  });
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.send("404");
+    res.sendFile(path.join(__dirname, "../client/public/index.html"));
   });
 };
--- server.js
+++ server.js
@@ -2,13 +2,15 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const PORT = process.env.PORT || 3000;
+const path = require("path");
+const PORT = process.env.PORT || 3001;
 // connect to MongoDB (mongod running? DB exist?)
 mongoose.connect("mongodb://localhost/mernstarter", { useNewUrlParser: true });
 let db = mongoose.connection;
 db.once("open", () => console.log("connected to the database"));
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 // no big URLs, assume it's JavaScript all the way down
+app.use(express.static(path.join(__dirname, "client/build")));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 // Requests go here
